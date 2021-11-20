7091c02d7fab71186f3c8b44141df27f79595663
Adam Houston <phazyy9@gmail.com>
2021-11-20 08:30:53 +0000

    #3: Adding clean architecture skeleton

---
+++ actions/actions.go
@@ -0,0 +1,6 @@
+package actions
+
+// UserLiquidityPools
+// Function here called with Filter (network, protocol, balance)
+// Uses passed "Chain" to call network
+// Returns (slice) LiquidityPool (custom struct containing LP info)
